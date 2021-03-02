7d5d06da3511366ebf6590e8d4bf3d9250b075a0
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-03-01 20:20:34 -0500

    Finished register routes and working on login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from 'express'
-import { validate } from 'class-validator'
+import { isEmpty, validate } from 'class-validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
 
 import { User } from '../entities/User'
 
@@ -39,7 +42,45 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
+const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body
+
+    try {
+        let errors: any = {}
+        if (isEmpty(username)) errors.username = 'Username must not be empty'
+        if (isEmpty(password)) errors.password = 'Password must not be empty'
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json(errors)
+        }
+
+        const user = await User.findOne({ username })
+
+        if (!user) return res.status(404).json({ error: 'User not found' })
+
+        const passwordMatches = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatches) {
+            return res.status(401).json({ password: 'Password is incorrect' })
+        }
+
+        const token = jwt.sign({ username }, '27oQPNLa')
+
+        res.set('Set-Cookie', cookie.serialize('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            maxAge: 3600,
+            path: '/'
+        }))
+
+        return res.json({ user, token })
+    } catch (err) {
+        
+    }
+}
+
 const router = Router()
 router.post('/register', register)
+router.post('/login', login)
 
 export default router
