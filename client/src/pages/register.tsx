9866b46e3583ebc3590ee45fb016e5cd2b561277
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-03-11 01:06:39 -0500

    feat: playing around with styles

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -26,7 +26,7 @@ export default function Register() {
             </Head>
             <div className="h-screen bg-gray-100">
                 <section>
-                    <div className="py-6 h-1/6">
+                    <div className="py-6 h-1/6" style={{zIndex: 9999}}>
                         <div className="container flex items-center justify-between mx-auto font-bold">
                             <div>
                                 <ul className="flex">
@@ -46,7 +46,7 @@ export default function Register() {
                             </div>
                             <div>
                                 <ul className="flex">
-                                    <li className="p-2 px-4 py-2 mx-4 font-bold bg-white rounded-full shadow-2xl hover:bg-blue-600 hover:text-white">
+                                    <li className="p-2 px-4 py-2 mx-4 font-bold bg-white rounded-full shadow-2xl hover:bg-indigo-600 hover:shadow-lg hover:text-white">
                                         <a href="#">Sign in</a>
                                     </li>
                                     <li className="p-2 mx-4">
@@ -57,31 +57,7 @@ export default function Register() {
                         </div>
                     </div>
                 </section>
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" id="loader">
-                    <defs>
-                        <filter id="goo">
-                            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-                            <feBlend in2="goo" in="SourceGraphic" result="mix" />
-                        </filter>
-                        <linearGradient id="MyGradient">
-                            <stop offset="5%" stop-color="#e310d1" />
-                            <stop offset="40%" stop-color="#34e8eb" />
-                            <stop offset="100%" stop-color="#34e8eb" />
-                        </linearGradient>
-                    </defs>
-                    <mask id="maska">
-                        <g className="blobs">
-                            <circle className="blob" cx="440" cy="250" r="30" transform="rotate(0) translate(0, 0) rotate(0)" />
-                            <circle className="blob" cx="500" cy="320" r="70" transform="rotate(0) translate(0, 0) rotate(0)" />
-                            <circle className="blob" cx="300" cy="390" r="40" transform="rotate(0) translate(0, 0) rotate(0)" />
-                            <circle className="blob" cx="380" cy="390" r="80" transform="rotate(0) translate(0, 0) rotate(0)" />
-                            <circle className="blob" cx="470" cy="450" r="20" transform="rotate(0) translate(0, 0) rotate(0)" />
-                        </g>
-                    </mask>
-                    <rect x="0" y="0" mask="url(#maska)" fill="url(#MyGradient)" width="200%" height="200%"></rect>
-                </svg>
-                <section className="flex h-5/6">
+                <section className="flex h-5/6" style={{zIndex: 10}}>
                     <div className="flex justify-center w-full lg:w-1/2">
                         <div className="self-center max-w-xl p-6">
                             <h1 className="mb-20 text-6xl font-bold leading-tight">
@@ -90,48 +66,64 @@ export default function Register() {
                             </h1>
                             <p className="mb-6 text-lg leading-relaxed">
                                 If you already have an account, <br />
-                                you can <a href="#" className="text-blue-700">sign in here</a>
+                                you can <a href="#" className="text-indigo-600">sign in here</a>
                             </p>
                         </div>
                     </div>
                     <div className="flex flex-col justify-center pl-6">
-                        <div className="w-70">
+                        <div className="w-96">
                             <form>
-                                <div className="mb-6">
-                                    <input
-                                        type="checkbox"
-                                        className="mr-1 cursor-pointer"
-                                        id="agreement"
-                                    />
-                                    <label htmlFor="agreement" className="text-xs cursor-pointer">
-                                        I agree to get emails about cool stuff on Readit
-                                </label>
-                                </div>
-                                <div className="mb-2">
+                                <div className="mb-8">
                                     <input
                                         type="email"
-                                        className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                                        className="w-full h-16 px-8 py-2 font-bold bg-indigo-100 rounded-xl"
                                         placeholder="Email"
                                     />
                                 </div>
-                                <div className="mb-2">
+                                <div className="mb-8">
                                     <input
                                         type="text"
-                                        className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                                        className="w-full h-16 px-8 py-2 font-bold bg-indigo-100 rounded-xl"
                                         placeholder="Username"
                                     />
                                 </div>
                                 <div className="mb-2">
                                     <input
                                         type="password"
-                                        className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                                        className="w-full h-16 px-8 py-2 font-bold bg-indigo-100 rounded-xl"
                                         placeholder="Password"
                                     />
                                 </div>
-                                <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+                                <div className="flex justify-end mb-12 text-sm font-bold text-gray-400 content-evenly continue-text">
+                                    <span>Forgot your password?</span>
+                                </div>
+                                <button className="w-full h-16 py-2 mb-4 mb-20 text-lg font-bold text-white bg-indigo-600 border border-blue-500 shadow-3xl rounded-xl">
                                     Sign Up
-            </button>
+                                </button>
                             </form>
+                            <div className="flex justify-center mb-6 font-bold text-gray-400 continue-text">
+                                <span>or continue with</span>
+                            </div>
+                            <div className="flex flex-row items-center justify-center">
+                                <div className="w-1/3 h-20 px-8 py-2 m-2 border rounded-xl hover:bg-white hover:shadow-2xl"
+                                    style={{
+                                        backgroundImage: "url('/images/icons8-google-52.png')",
+                                        backgroundRepeat: "no-repeat", backgroundPosition: "center center"
+                                    }}>
+                                </div>
+                                <div className="w-1/3 h-20 px-8 py-2 m-2 border rounded-xl hover:bg-white hover:shadow-2xl"
+                                    style={{
+                                        backgroundImage: "url('/images/icons8-apple-logo-52.png')",
+                                        backgroundRepeat: "no-repeat", backgroundPosition: "center center"
+                                    }}>
+                                </div>
+                                <div className="w-1/3 h-20 px-8 py-2 m-2 border rounded-xl hover:bg-white hover:shadow-2xl"
+                                    style={{
+                                        backgroundImage: "url('/images/icons8-facebook-52.png')",
+                                        backgroundRepeat: "no-repeat", backgroundPosition: "center center"
+                                    }}>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
